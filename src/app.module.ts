b1d61bf47e1bf48ff6eea464c6ed18202acca143
fara0000 <fi@livereach.com>
2022-06-07 02:24:37 +0300

    initialized nest js

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [TypeOrmModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
+import {IsEmail, IsNotEmpty, IsString, Max, Min} from "class-validator";
+
+@Entity("user")
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    @IsString()
+    @IsNotEmpty()
+    @Min(4)
+    @Max(50)
+    name: string;
+
+    @Column({ nullable: false })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @Column({ nullable: false })
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
